5a418eab64138daec2c67ba21fee47efdebebfce
NyGold <nycosteixeira@gmail.com>
2024-11-28 17:29:06 -0300

    num sei

--- portas_logicas.py
+++ portas_logicas.py
@@ -1,6 +1,3 @@
-vb1 = int(input())
-vb2 = int(input())
-
 # codigo que vai para o site
 def porta_e(x, y):
     return 1 if (x == 1 and y == 1) else 0
@@ -13,6 +10,7 @@ def porta_not(x):
         return 0
     else:
         return 1
+
 def porta_nand(x, y):
     return porta_not(porta_ou(x, y))
 
@@ -22,13 +20,8 @@ def porta_nor(x, y):
 def porta_xor(x, y):
     return 0 if x == y else 1
 
-print(f"vb1 = {vb1}, vb2 = {vb2}")
-
-print()
-
-print(f"porta e: {porta_e(vb1, vb2)}")
-print(f"porta ou: {porta_ou(vb1, vb2)}")
-print(f"porta not: p/ vb1: {porta_not(vb1)} p/ vb2: {porta_not(vb2)}")
-print(f"porta nand: {porta_nand(vb1, vb2)}")
-print(f"porta nor: {porta_nor(vb1, vb2)}")
-print(f"porta xor: {porta_xor(vb1, vb2)}")
+# testes
+print(porta_e(0, 0))
+print(porta_e(0, 1))
+print(porta_e(1, 0))
+print(porta_e(1, 1))
