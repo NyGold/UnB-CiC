cb535f710bdade8069e2054e0934beb74e3bc782
NyGold <nycosteixeira@gmail.com>
2024-11-28 13:41:26 -0300

    portas_logicas.py ultimo exercicio, que está quebrado*

--- portas_logicas.py
+++ portas_logicas.py
@@ -1,13 +1,34 @@
-vb1 = input()
-vb2 = input()
+vb1 = int(input())
+vb2 = int(input())
 
 # codigo que vai para o site
-def porta_not(vb):
-    return not vb
+def porta_e(x, y):
+    return 1 if (x == 1 and y == 1) else 0
 
-def porta_nand(vb1, vb2):
-    if not (vb1 == 1 and vb2 == 1):
+def porta_ou(x, y):
+    return 1 if (x == 1 or y == 1) else 0
+
+def porta_not(x):
+    if x == 1:
         return 0
+    else:
+        return 1
+def porta_nand(x, y):
+    return porta_not(porta_ou(x, y))
 
-print(porta_nand())
-print(porta_not())
+def porta_nor(x, y):
+    return porta_not(porta_ou(x, y))
+
+def porta_xor(x, y):
+    return 0 if x == y else 1
+
+print(f"vb1 = {vb1}, vb2 = {vb2}")
+
+print()
+
+print(f"porta e: {porta_e(vb1, vb2)}")
+print(f"porta ou: {porta_ou(vb1, vb2)}")
+print(f"porta not: p/ vb1: {porta_not(vb1)} p/ vb2: {porta_not(vb2)}")
+print(f"porta nand: {porta_nand(vb1, vb2)}")
+print(f"porta nor: {porta_nor(vb1, vb2)}")
+print(f"porta xor: {porta_xor(vb1, vb2)}")
