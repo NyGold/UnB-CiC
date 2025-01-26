8118bb88a829af9be5551de84c2bd21f84487ce0
Nycollas S. Teixeira <nycosteixeira@gmail.com>
2025-01-26 14:41:48 -0300

    ainda não deu certo

--- estrutura_de_dados/questao5_prat.py
+++ estrutura_de_dados/questao5_prat.py
@@ -15,8 +15,17 @@ fileiras, cadeiras = str2int(fileiras_e_cadeiras)
 
 for f in range(fileiras):
     fila = input().split()
-    
+    distancia = 0
 
+    # aqui que vai contar a distancia maxima
+    for c in fila: # roda toda a fileira procurando por ocorrencia de 0
+        if c == '0':
+            distancia = fila[fila.index(c):fila.index('1')].count('0') 
+            maior_distancia = distancia if distancia > maior_distancia else maior_distancia                
+        else:
+            distancia = (fila[(fila.index(c)+1):fila[fila.index(c)+1:].index('1')].count('0') // 2) + 1
+            maior_distancia = distancia if distancia > maior_distancia else maior_distancia
+        
     maior_distancia = distancia if distancia > maior_distancia else maior_distancia
     
 print(maior_distancia) 
