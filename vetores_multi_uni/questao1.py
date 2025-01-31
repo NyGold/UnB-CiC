d46edbf02a88990dd2f475f108ba1d4dc13a8fb4
Nycollas S. Teixeira <nycosteixeira@gmail.com>
2025-01-31 11:50:19 -0300

    desgraça

--- vetores_multi_uni/questao1.py
+++ vetores_multi_uni/questao1.py
@@ -8,3 +8,22 @@ for _ in range(qtde_projeteis):
 
 profundidade = [[0] * tamanho_terreno_x] * tamanho_terreno_y
 
+for bomba in range(qtde_projeteis):
+    coodernadas_projetil_x, coodernadas_projetil_y = coodernadas_projetil[bomba]
+
+    for x in range(tamanho_terreno_x-1):
+        profundidade[x][coodernadas_projetil_x] += 1
+
+    for y in range(tamanho_terreno_y-1):
+        profundidade[coodernadas_projetil_y][y] += 1
+
+    profundidade[coodernadas_projetil_x-1][coodernadas_projetil_y-1] -= 1
+
+#     y=1 2 3
+# x= i  0 1 2
+# 1  0 [0 0 0]    x    0       1       2
+# 2  1 [0 0 0]    y  0 1 2   0 1 2   0 1 2
+# 3  2 [0 0 0]  ou [[0,0,0],[0,0,0],[0,0,0]]
+
+for i in profundidade:
+    print(i)
