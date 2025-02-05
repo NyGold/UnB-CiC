a92e90b37d5a831564faed75d6d33302eb19e53c
Nycollas S. Teixeira <nycosteixeira@gmail.com>
2025-02-05 13:36:03 -0300

    não sei mais o que fazer

--- vetores_multi_uni/questao4.py
+++ vetores_multi_uni/questao4.py
@@ -2,7 +2,7 @@ def main():
     N, M, f = [int(i) for i in input().split()]
 
     A = []
-    B = []
+    B = [[0] * (M//f) for _ in range(N//f)]
     res = []
 
     for _ in range(N):
@@ -12,15 +12,11 @@ def main():
         res = A
         num_max = max(max(linha) for linha in A)
         B.append(num_max)
-        for i in B:
-            print(i)
         return
 
     for i in range(len(A)//f):
         res.append(A[i][:len(A[0])//f])
-        
-
-    for i in res:
-        print(i)
+        num_max = max(max(linha) for linha in res)
+        print(num_max)
 
 main()
